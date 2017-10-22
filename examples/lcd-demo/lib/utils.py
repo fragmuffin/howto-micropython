4a3fb9a89c5f29fecc4da9b4dd27d3cafb292eb7
Peter Boin <Peter.Boin@gmail.com>
2017-10-23 02:55:04 +1100

    led indicates frame buffering

--- examples/lcd-demo/lib/utils.py
+++ examples/lcd-demo/lib/utils.py
@@ -1,5 +1,5 @@
 import framebuf
-
+import pyb
 
 # ------------ Font Utilities ------------
 _font_heights = {
@@ -15,22 +15,38 @@ def font_height(index, scale=0):
 
 # ------------ LCD Utilities ------------
 class RestoreFrameContext():
-    def __init__(self, lcd):
+    def __init__(self, lcd, led=True):
         self.lcd = lcd
         self.buffer = None
 
+        # use LED to show when frame buffering is occuring
+        self.led = None
+        if led:
+            self.led = pyb.LED(3)
+
+    def set_led(self, val):
+        if self.led:
+            if val:
+                self.led.intensity(10)
+            else:
+                self.led.off()
+
     def __enter__(self):
         # save buffer
+        self.set_led(True)
         self.buffer = bytearray(2 * self.lcd.w * self.lcd.h)
         self.lcd.screen_dump(self.buffer)
+        self.set_led(False)
         return self.buffer
 
     def __exit__(self, *args):
+        self.set_led(True)
         self.lcd.set_pos(0, 0)
         self.lcd.screen_load(self.buffer)
+        self.set_led(False)
 
 
-def restore_framebuffer(lcd):
+def restore_framebuffer(*args, **kwargs):
     """
     Record and restore LCD's frame buffer after context block.
     example usage:
@@ -40,4 +56,4 @@ def restore_framebuffer(lcd):
             time.sleep(1)
     :param lcd: lcd160cr.LCD160CR instance
     """
-    return RestoreFrameContext(lcd)
+    return RestoreFrameContext(*args, **kwargs)
